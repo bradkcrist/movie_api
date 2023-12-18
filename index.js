ac33aaebd9fda787d8c2218f79bd66ecc78a05a3
bradkcrist <bradkcrist08@gmail.com>
2023-12-18 05:52:31 -0700

    Rewrited my node into express format

--- index.js
+++ index.js
@@ -1,13 +1,41 @@
 const express = require('express');
+morgan = require('morgan');
+(fs = require('fs')), (path = require('path'));
+
 const app = express();
 
-const http = require('http');
+app.get('/myMovie_APi', (req, res) => {
+  res.send('My Top 10 Movies!');
+});
 
-http
-  .createServer((request, response) => {
-    response.writeHead(200, { 'Content-Type': 'text/plain' });
-    response.end('Welcome to my book club!\n');
+const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
+  flags: 'a',
+});
+
+app.use(morgan('combined', { stream: accessLogStream }));
+
+app.get('/', (req, res) => {
+  res.send('Welcome to my app!');
+});
+
+app.use(express.static('public'));
+
+const bodyParser = require('body-parser'),
+  methodOverride = require('method-override');
+
+app.use(
+  bodyParser.urlencoded({
+    extended: true,
   })
-  .listen(8080);
+);
 
-console.log('My first Node test server is running on port 8080.');
+app.use(bodyParser.json());
+app.use(methOverride());
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Code broke!');
+});
+
+app.listen(8080);
+console.log('Your app is running on Port 8080.');
