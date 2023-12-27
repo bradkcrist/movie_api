35e78a94ad8355752f778bb36b5681636b8b6f2c
bradkcrist <bradkcrist08@gmail.com>
2023-12-27 05:01:47 -0700

    Removed all my old get request and put in new get request, delete request, post request and put request about my movie

--- index.js
+++ index.js
@@ -1,26 +1,157 @@
-const express = require('express');
-morgan = require('morgan');
-(fs = require('fs')), (path = require('path'));
+const express = require("express");
+morgan = require("morgan");
+const uuid = require("uuid");
+const bodyParser = require("body-parser");
+(fs = require("fs")), (path = require("path"));
 
 const app = express();
+app.use(bodyParser.json());
 
-app.get('/myMovie_API', (req, res) => {
-  res.send('My Top 10 Movies!');
+let users = [
+  {
+    id: 1,
+    name: "User1",
+    favoriteMovie: [],
+  },
+  {
+    id: 2,
+    name: "User2",
+    favoriteMovie: [],
+  },
+];
+
+let movies = [
+  {
+    Title: "Hobbit",
+    Plot: "A hobbit named Bilbo Baggins joins a crew of dwarves to reclaim there home on the Lonley Mountains.",
+    Director: {
+      Name: "Peter Jackson",
+      Birth: "October 31 1961",
+    },
+    Genre: {
+      Name: "Fantasy",
+    },
+  },
+];
+
+//Create
+app.post("/users", (req, res) => {
+  const newUser = req.body;
+
+  if (newUser.name) {
+    newUser.id = uuid.v4();
+    users.push(newUser);
+    res.status(201).json(newUser);
+  } else {
+    res.status(400).send("need user name");
+  }
 });
 
-const accessLogStream = fs.createWriteStream(path.join(__dirname, 'log.txt'), {
-  flags: 'a',
+//Update
+app.put("/users/:id", (req, res) => {
+  const { id } = req.params;
+  const updatedUser = req.body;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.name = updatedUser.name;
+    res.status(200).json(user);
+  } else {
+    res.status(400).send("no such user");
+  }
 });
 
-app.use(morgan('combined', { stream: accessLogStream }));
+//Create
+app.post("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
 
-app.get('/', (req, res) => {
-  res.send('Welcome to my Top 10!');
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.favoriteMovie.push(movieTitle);
+    res.status(200).send(`${movieTitle} has been added to user ${id}'s array`);
+  } else {
+    res.status(400).send("no such movie");
+  }
 });
 
-app.use(express.static('public'));
+//Delete
+app.delete("/users/:id/:movieTitle", (req, res) => {
+  const { id, movieTitle } = req.params;
 
-const bodyParser = require('body-parser');
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user.favoriteMovie = user.filter(title !== movieTitle);
+    res.status(200).send(`${movieTitle} has been removed from user ${id}'s array`);
+  } else {
+    res.status(400).send("failed to remove movie");
+  }
+});
+
+//Delete
+app.delete("/users/:id", (req, res) => {
+  const { id } = req.params;
+
+  let user = users.find(user => user.id == id);
+
+  if (user) {
+    user = users.filter(user => user.id != id);
+    res.status(200).send(`user ${id} has been deleted`);
+  } else {
+    res.status(400).send(" failed to remove user");
+  }
+});
+
+//Read
+app.get("/movies", (req, res) => {
+  res.status(200).json(movies);
+});
+
+//Read
+app.get("/movies/:title", (req, res) => {
+  const { title } = req.params;
+  const movie = movies.find(movie => movie.Title === title);
+
+  if (movie) {
+    res.status(200).json(movies);
+  } else {
+    res.status(400).send("No movie");
+  }
+});
+
+//Read
+app.get("/movies/genre/:genreName", (req, res) => {
+  const { genreName } = req.params;
+  const genre = movies.find(movie => movie.Genre.Name === genreName);
+
+  if (genre) {
+    res.status(200).json(genre);
+  } else {
+    res.status(400).send("No genre");
+  }
+});
+
+//Read
+app.get("/movies/directors/:directorName", (req, res) => {
+  const { directorName } = req.params;
+  const director = movies.find(movie => movie.Director.Name === directorName);
+
+  if (director) {
+    res.status(200).json(director);
+  } else {
+    res.status(400).send("No Director");
+  }
+});
+
+const accessLogStream = fs.createWriteStream(path.join(__dirname, "log.txt"), {
+  flags: "a",
+});
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
+app.use(express.static("public"));
 
 app.use(
   bodyParser.urlencoded({
@@ -28,12 +159,10 @@ app.use(
   })
 );
 
-app.use(bodyParser.json());
-
 app.use((err, req, res, next) => {
   console.error(err.stack);
-  res.status(500).send('Code broke!');
+  res.status(500).send("Code broke!");
 });
 
 app.listen(8080);
-console.log('Your app is running on Port 8080.');
+console.log("Your app is running on Port 8080.");
